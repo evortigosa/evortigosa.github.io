d29fbb0fa7a596f224d74aefb17082d9d2a93003
Evandro Ortigossa <evortigosa@gmail.com>
2018-01-24 15:08:53 -0200

    Add files via upload

--- pollution/difference.js
+++ pollution/difference.js
@@ -83,23 +83,25 @@ function draw_difference(data, view) {
 	canvas.datum(data);
 
 	canvas.append("clipPath")
-		.attr("id", "clip-above")
-		.append("path")
-		.attr("d", area.y0(0));
-
-	canvas.append("clipPath")
 		.attr("id", "clip-below")
 		.append("path")
 		.attr("d", area.y0(height));
 
+	canvas.append("clipPath")
+		.attr("id", "clip-above")
+		.append("path")
+		.attr("d", area.y0(0));
+
 	canvas.append("path")
-		.attr("class", "area above")
-		.attr("clip-path", "url(#clip-above)")
+		.attr("class", "area_below")
+		.attr("clip-path", "url(#clip-below)")
+		.style("fill", color_group(4))
 		.attr("d", area.y0(function(d) { return y_scale(d.down); }));
 
 	canvas.append("path")
-		.attr("class", "area below")
-		.attr("clip-path", "url(#clip-below)")
+		.attr("class", "area_above")
+		.attr("clip-path", "url(#clip-above)")
+		.style("fill", color_group(5))
 		.attr("d", area.y0(function(d) { return y_scale(d.down); }));
 
 	canvas.append("path")
